4f3d43a1b39187c2b626906ce4f79f4dffb895fc
Stefan Magnuson <styrmis@gmail.com>
2023-05-07 12:23:44 +0100

    Apply rubocop to embeddings.rb

--- embeddings.rb
+++ embeddings.rb
@@ -2,34 +2,34 @@ require 'dotenv'
 require 'ruby/openai'
 require 'csv'
 
-Dotenv.load()
+Dotenv.load
 
 openai = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
 
 text_array = []
 embedding_array = []
 
-Dir.glob("training-data/*.txt") do |file|
-  text = File.read(file).dump()
+Dir.glob('training-data/*.txt') do |file|
+  text = File.read(file).dump
   text_array << text
 end
 
 text_array.each do |text|
   response = openai.embeddings(
     parameters: {
-      model: "text-embedding-ada-002",
+      model: 'text-embedding-ada-002',
       input: text
     }
   )
-  
+
   embedding = response['data'][0]['embedding']
 
-  embedding_hash = {embedding: embedding, text: text}
+  embedding_hash = { embedding: embedding, text: text }
   embedding_array << embedding_hash
 end
 
-CSV.open("embeddings.csv", "w") do |csv|
-  csv << [:embedding, :text]
+CSV.open('embeddings.csv', 'w') do |csv|
+  csv << %i[embedding text]
   embedding_array.each do |obj|
     csv << [obj[:embedding], obj[:text]]
   end
