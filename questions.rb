14ca3cf76068d59e7dca1832a516c83d5458da99
Stefan Magnuson <styrmis@gmail.com>
2023-05-07 12:40:59 +0100

    Fix linting issues in questions.rb

--- questions.rb
+++ questions.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'dotenv'
 require 'ruby/openai'
 require 'csv'
 require 'cosine_similarity'
 
-Dotenv.load()
+Dotenv.load
 
 openai = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
 
-puts "Welcome to the Sterling Parts AI Knowledge Base. How can I help you?"
+puts 'Welcome to the Sterling Parts AI Knowledge Base. How can I help you?'
 question = gets
 
 response = openai.embeddings(
   parameters: {
-    model: "text-embedding-ada-002",
+    model: 'text-embedding-ada-002',
     input: question
   }
 )
@@ -21,22 +23,20 @@ question_embedding = response['data'][0]['embedding']
 
 similarity_array = []
 
-CSV.foreach("embeddings.csv", headers: true) do |row|
+CSV.foreach('embeddings.csv', headers: true) do |row|
   text_embedding = JSON.parse(row['embedding'])
   similarity_array << cosine_similarity(question_embedding, text_embedding)
 end
 
 index_of_max = similarity_array.index(similarity_array.max)
-original_text = ""
+original_text = ''
 
-CSV.foreach("embeddings.csv", headers: true).with_index do |row, rowno|
-  if rowno == index_of_max
-    original_text = row['text']
-  end
+CSV.foreach('embeddings.csv', headers: true).with_index do |row, rowno|
+  original_text = row['text'] if rowno == index_of_max
 end
 
-prompt = 
-"You are an AI assistant. You work for Sterling Parts which is a car parts online store located in Australia.
+prompt =
+  "You are an AI assistant. You work for Sterling Parts which is a car parts online store located in Australia.
 You will be asked questions from a customer and will answer in a helpful and friendly manner.
 
 You will be provided company information from Sterline Parts under the [Article] section. The customer question
@@ -51,10 +51,10 @@ If the users question is not answered by the article you will respond with 'I'm 
 
 response = openai.completions(
   parameters: {
-    model: "text-davinci-003",
+    model: 'text-davinci-003',
     prompt: prompt,
     temperature: 0.2,
-    max_tokens: 500,
+    max_tokens: 500
   }
 )
 
